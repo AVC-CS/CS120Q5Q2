0586e270d68d5f2d5f6ac00ca8af033e9befe11f
Kyu Lee <kwooler@gmail.com>
2026-03-12 01:49:01 -0700

    Solution code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
-#include    <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-   int N, M;
-   
-   cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
-   cin >> N >> M;
-   /* Complete your code */
-  
+    int N, M;
+
+    cout << "Enter your base number: ";
+    cin >> N;
+    cout << "Enter your exponent: ";
+    cin >> M;
+
+    cout << N << " to power number: ";
+
+    // TODO
+    int result = 1;
+    for (int i = 0; i <= M; i++) {
+        if (i > 0) cout << ", ";
+        cout << result;
+        result *= N;
+    }
+    // END TODO
+
+    cout << endl;
+
+    return 0;
 }
